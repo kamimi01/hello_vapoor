0edb96c56168655eb6fb8b52848bdb4d8b94f2d4
mika.urakawa <awakaru0122@gmail.com>
2023-04-05 07:30:05 +0900

    feat: first request with valid jwt

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,8 +1,27 @@
 import Vapor
+import JWT
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    // Add HMAC with SHA-256 signer.
+    app.jwt.signers.use(.hs256(key: "secret"))
+
+    app.get("me") { req -> HTTPStatus in
+        let payload = try req.jwt.verify(as: TestPayload.self)
+        print(payload)
+        return .ok
+    }
+
+    app.post("login") { req -> [String: String] in
+        let payload = TestPayload(
+            subject: "vapor",
+            expiration: .init(value: .distantFuture),
+            isAdmin: true
+        )
+
+        print(try! req.jwt.sign(payload))
+        return try [
+            "token": req.jwt.sign(payload)
+        ]
     }
 
     app.get("hello") { req async -> String in
