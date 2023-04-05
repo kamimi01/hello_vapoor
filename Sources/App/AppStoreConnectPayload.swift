b9a7bbbaa21ab1e2fb9cf3b694695a7587586016
mika.urakawa <awakaru0122@gmail.com>
2023-04-06 07:15:15 +0900

    feat: create jwt for app store connect api

---
+++ Sources/App/AppStoreConnectPayload.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by mikaurakawa on 2023/04/06.
+//
+
+import Foundation
+import JWT
+
+struct AppStoreConnectPayload: JWTPayload {
+    enum CodingKeys: String, CodingKey {
+        case issuer = "iss"
+        case issuedAtClaim = "ias"
+        case expiration = "exp"
+        case audience = "aud"
+    }
+
+    var issuer: IssuerClaim
+    var issuedAtClaim: IssuedAtClaim
+    var expiration: ExpirationClaim
+    var audience: AudienceClaim
+
+    // 追加の検証ロジックを実行する
+    func verify(using signer: JWTSigner) throws {
+        try expiration.verifyNotExpired(currentDate: Date())
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,28 +3,46 @@ import JWT
 
 func routes(_ app: Application) throws {
     // Add HMAC with SHA-256 signer.
-    app.jwt.signers.use(.hs256(key: "secret"))
+//    app.jwt.signers.use(.hs256(key: "secret"))  // これがあるとJWTがHS256アルゴリズムになるので、一旦コメントアウト
+//
+//    app.get("me") { req -> HTTPStatus in
+//        let payload = try req.jwt.verify(as: TestPayload.self)
+//        print(payload)
+//        return .ok
+//    }
+//
+//    app.post("login") { req -> [String: String] in
+//        let payload = TestPayload(
+//            subject: "vapor",
+//            expiration: .init(value: .distantFuture),
+//            isAdmin: true
+//        )
+//
+//        print(try! req.jwt.sign(payload))
+//        return try [
+//            "token": req.jwt.sign(payload)
+//        ]
+//    }
+//
+//    app.get("hello") { req async -> String in
+//        "Hello, world!"
+//    }
 
-    app.get("me") { req -> HTTPStatus in
-        let payload = try req.jwt.verify(as: TestPayload.self)
-        print(payload)
-        return .ok
-    }
+    app.get("appstoreconnect") { req -> [String: String] in
+        let privateKey = privateKey
 
-    app.post("login") { req -> [String: String] in
-        let payload = TestPayload(
-            subject: "vapor",
-            expiration: .init(value: .distantFuture),
-            isAdmin: true
+        let payload = AppStoreConnectPayload(
+            issuer: .init(value: issuerID),
+            issuedAtClaim: .init(value: Date()),
+            expiration: .init(value: Calendar.current.date(byAdding: .minute, value: 20, to: Date()) ?? Date()),
+            audience: .init(value: ["appstoreconnect-v1"])
         )
 
-        print(try! req.jwt.sign(payload))
-        return try [
-            "token": req.jwt.sign(payload)
-        ]
-    }
+        let key = try ECDSAKey.private(pem: privateKey)
+        try app.jwt.signers.use(.es256(key: key))
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+        return try [
+            "token": req.jwt.sign(payload, kid: keyID)
+        ]
     }
 }
